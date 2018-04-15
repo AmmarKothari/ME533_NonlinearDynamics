b4aa4ae6977776f72d8425881d5ccc11e40f7ba2
Ammar <amarsbars@gmail.com>
2018-04-14 21:47:49 -0700

    HW1 Problem 2

--- HWs/HW1.m
+++ HWs/HW1.m
@@ -37,3 +37,27 @@ end
 xlim([grid_min, grid_max])
 ylim([grid_min, grid_max])
 hold off;
+
+
+%% Problem 2
+r_dot = @(r,t) r*(r^2-1) *sin(1/(r^2-1));
+X = -2:0.01:2;
+Y = [];
+for x=X
+    Y = [Y, r_dot(x)];
+end
+% plot(X,Y)
+for r_0 = logspace(-3,0.1,10)
+    r_0 = 0.9999999999999;
+    r_all = [];
+    r = r_0;
+    dt  = 0.1;
+    T = 1:dt:105;
+    for t = T
+        r = r + r_dot(r,t)*dt;
+        r_all = [r_all, r];
+    end
+    plot(T,r_all);
+    fprintf('Starting Radius: %s, Final Value: %s \n',r_0, r_all(end))
+end
+

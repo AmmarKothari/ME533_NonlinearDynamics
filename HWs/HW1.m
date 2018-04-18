880af86335bd8ebe1dcc591ac1b7d3593744b89e
Ammar <amarsbars@gmail.com>
2018-04-17 19:29:24 -0700

    finished HW1

--- HWs/HW1.m
+++ HWs/HW1.m
@@ -40,24 +40,55 @@ hold off;
 
 
 %% Problem 2
+
+% prove limit cycles
 r_dot = @(r,t) r*(r^2-1) *sin(1/(r^2-1));
-X = -2:0.01:2;
-Y = [];
-for x=X
-    Y = [Y, r_dot(x)];
-end
-% plot(X,Y)
-for r_0 = logspace(-3,0.1,10)
-    r_0 = 0.9999999999999;
+r_limit_cyle = @(n) sqrt(1/(n*pi) + 1);
+r_dot_all = [];
+figure(1); cla; hold on;
+for pi_multiples = 10:10
+    r_0 = r_limit_cyle(pi_multiples);
     r_all = [];
     r = r_0;
-    dt  = 0.1;
-    T = 1:dt:105;
+    dt  = 0.01;
+    T = 0:dt:10;
     for t = T
-        r = r + r_dot(r,t)*dt;
+        r_dot_all = [r_dot_all, r_dot(r,t)]; 
+        r = r + r_dot_all(end)*dt;
         r_all = [r_all, r];
     end
-    plot(T,r_all);
+%     plot(T,r_all);
+    plot(r_all, r_dot_all, 'rx-');
     fprintf('Starting Radius: %s, Final Value: %s \n',r_0, r_all(end))
+    pause(1)
 end
+
+%% stability
+disturbance = 1e-3;
+for pi_multiples = 1:10
+    for dr = [-1, 1]
+        r_dot_temp = r_dot(r_limit_cyle(pi_multiples)+dr*disturbance);
+        fprintf('Multiple: %d, Disturbance: %d, r_dot: %0.20f+%fj\n', pi_multiples, dr, real(r_dot_temp), imag(r_dot_temp))
+    end
+end
+%%
+y_dot = @(y,u) (sqrt(2*u*y) - 10*y)*[1;-1];
+u_all = 0:1:2;
+y_all = 0:0.001:0.3;
+figure(1); cla; hold on;
+for u = u_all
+    y_dot_all = [];
+    for y = y_all
+        y_dot_all = [y_dot_all; y_dot(y,u)];
+    end
+    plot(y_all, y_dot_all(2:2:end), 'rx-')
+    plot(y_all, y_dot_all(1:2:end), 'bx-')
+end
+hold off
+
+
+
+
+
+
 

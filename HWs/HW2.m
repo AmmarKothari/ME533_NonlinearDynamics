3d6d7fb47dc4274fbc7b43395e427055393e6cc1
Ammar <amarsbars@gmail.com>
2018-04-30 15:22:57 -0700

    finished first pass through homework 2

--- HWs/HW2.m
+++ HWs/HW2.m
@@ -58,7 +58,7 @@ axis equal
 
 %% Problem 2
 % (i)
-xs = -1e0:1e-3:1e0;
+xs = -1e0:1e-5:1e0;
 x1 = 0*xs;
 x2 = 0*xs;
 V = 0*xs;
@@ -76,3 +76,46 @@ plot(xs, V, 'r-', xs, V_dot, 'b-')
 
 f = @(x) -x^3 + sin(x)^4;
 fzero(f, 1)
+
+%% Problem 3
+dt = 1e-1;
+t_total = 5;
+figure(1); cla; hold on;
+% for a = logspace(-3,1)
+    a = 1e-2;
+    b = 1e-0; c = 1;
+    v_dot = @(v) -2*a*abs(v)*v - b*v + c;
+    v_func = @(dt, x) v_dot(x(1));
+    [t, y] = ode45(v_func, 0:dt:t_total, 1);
+    plot(t, y(:,1), 'ro-', t(1:end-1), diff(y), 'bx-')
+    legend('Velocity', 'Acceleration')
+% end
+
+% figure(2); cla; hold on;
+% for b = logspace(-3,0)
+%     a = 1e-2;
+% %     b = 1e-3;
+%     c = 1e-2;
+%     v_dot = @(v) -2*a*abs(v)*v - b*v + c;
+%     v_func = @(dt, x) [v_dot(x(1)); 0];
+%     [t, y] = ode45(v_func, 0:dt:t_total, [0; 0]);
+%     plot(t, y(:,1), 'o-', t, y(:,2), 'bx-')
+% end
+
+%% Problem 4
+%% a
+syms t
+A = [-10, exp(3*t);
+    0, -2];
+eig(A + transpose(A))
+eigs = @(t) [(exp(6*t) + 64)^(1/2) - 12;
+            - (exp(6*t) + 64)^(1/2) - 12];
+
+%% b
+syms t
+A = [-1, 2*sin(t);
+    0, -(t+1)];
+
+eig(A + transpose(A))
+eigs = @(t) [(4*sin(t)^2 + t^2)^(1/2) - t - 2;
+             - t - (4*sin(t)^2 + t^2)^(1/2) - 2];
